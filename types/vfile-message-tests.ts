ac1a68570cbb286f0abc63395c03f13687dc0d37
Junyoung Choi <fluke8259@gmail.com>
2018-12-14 19:38:24 +0900

    Enable prettier

--- types/vfile-message-tests.ts
+++ types/vfile-message-tests.ts
@@ -1,10 +1,10 @@
-import vfileMessage = require('vfile-message');
+import vfileMessage = require('vfile-message')
 
-const message = vfileMessage('Error!');
-vfileMessage(new Error());
+const message = vfileMessage('Error!')
+vfileMessage(new Error())
 vfileMessage('Error!', {
   type: 'random node'
-});
+})
 vfileMessage('Error!', {
   start: {
     line: 1,
@@ -14,19 +14,19 @@ vfileMessage('Error!', {
     line: 1,
     column: 1
   }
-});
+})
 vfileMessage('Error!', {
   line: 1,
   column: 1
-});
-vfileMessage('Error!', undefined, 'test');
+})
+vfileMessage('Error!', undefined, 'test')
 
-message.file = '';
-message.name = '';
-message.reason = '';
-message.message = '';
-message.stack = '';
-message.fatal = null;
-message.fatal = true;
-message.column = 1;
-message.line = 1;
+message.file = ''
+message.name = ''
+message.reason = ''
+message.message = ''
+message.stack = ''
+message.fatal = null
+message.fatal = true
+message.column = 1
+message.line = 1
