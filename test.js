109f0f95baa26c9fd65ddc12df95e222a0c0fc6e
Titus Wormer <tituswormer@gmail.com>
2022-11-17 10:39:48 +0100

    Use Node test runner

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
 /**
- * @typedef {import('unist').Position} Position
- * @typedef {import('unist').Point} Point
  * @typedef {import('mdast').Root} Root
  */
 
+import assert from 'node:assert/strict'
 import path from 'node:path'
-import test from 'tape'
+import test from 'node:test'
 import {VFileMessage} from './index.js'
 
 /* eslint-disable no-undef */
@@ -46,32 +45,40 @@ try {
 }
 /* eslint-enable no-undef */
 
-test('VFileMessage(reason[, place][, origin])', function (t) {
-  t.ok(new VFileMessage('') instanceof Error, 'should return an Error')
+test('VFileMessage(reason[, place][, origin])', function () {
+  assert.ok(new VFileMessage('') instanceof Error, 'should return an Error')
 
   const m1 = new VFileMessage('Foo')
 
-  t.equal(m1.name, '1:1')
-  t.equal(m1.file, '')
-  t.equal(m1.reason, 'Foo')
-  t.equal(m1.ruleId, null)
-  t.equal(m1.source, null)
-  t.equal(m1.stack, '')
-  t.equal(m1.fatal, null)
-  t.equal(m1.line, null)
-  t.equal(m1.column, null)
-  t.deepEqual(m1.position, {
+  assert.equal(m1.name, '1:1')
+  assert.equal(m1.file, '')
+  assert.equal(m1.reason, 'Foo')
+  assert.equal(m1.ruleId, null)
+  assert.equal(m1.source, null)
+  assert.equal(m1.stack, '')
+  assert.equal(m1.fatal, null)
+  assert.equal(m1.line, null)
+  assert.equal(m1.column, null)
+  assert.deepEqual(m1.position, {
     start: {line: null, column: null},
     end: {line: null, column: null}
   })
 
-  t.equal(String(m1), '1:1: Foo', 'should have a pretty `toString()` message')
+  assert.equal(
+    String(m1),
+    '1:1: Foo',
+    'should have a pretty `toString()` message'
+  )
 
   const m2 = new VFileMessage(exception)
 
-  t.equal(m2.message, 'variable is not defined', 'should accept an error (1)')
+  assert.equal(
+    m2.message,
+    'variable is not defined',
+    'should accept an error (1)'
+  )
 
-  t.equal(
+  assert.equal(
     String(m2.stack || '').split('\n')[0],
     'ReferenceError: variable is not defined',
     'should accept an error (2)'
@@ -79,9 +86,9 @@ test('VFileMessage(reason[, place][, origin])', function (t) {
 
   const m3 = new VFileMessage(changedMessage)
 
-  t.equal(m3.message, 'foo', 'should accept a changed error (1)')
+  assert.equal(m3.message, 'foo', 'should accept a changed error (1)')
 
-  t.equal(
+  assert.equal(
     String(m3.stack || '').split('\n')[0],
     'ReferenceError: foo',
     'should accept a changed error (2)'
@@ -89,9 +96,13 @@ test('VFileMessage(reason[, place][, origin])', function (t) {
 
   const m4 = new VFileMessage(multilineException)
 
-  t.equal(m4.message, 'foo\nbar\nbaz', 'should accept a multiline error (1)')
+  assert.equal(
+    m4.message,
+    'foo\nbar\nbaz',
+    'should accept a multiline error (1)'
+  )
 
-  t.equal(
+  assert.equal(
     String(m4.stack || '')
       .split('\n')
       .slice(0, 3)
@@ -110,15 +121,19 @@ test('VFileMessage(reason[, place][, origin])', function (t) {
 
   const m5 = new VFileMessage('test', literalNode)
 
-  t.deepEqual(m5.position, literalNode.position, 'should accept a node (1)')
-  t.equal(String(m5), '2:3-2:5: test', 'should accept a node (2)')
-  t.equal(
+  assert.deepEqual(
+    m5.position,
+    literalNode.position,
+    'should accept a node (1)'
+  )
+  assert.equal(String(m5), '2:3-2:5: test', 'should accept a node (2)')
+  assert.equal(
     String(new VFileMessage('test', {type: 'x'})),
     '1:1-1:1: test',
     'should accept a node (3)'
   )
 
-  t.equal(
+  assert.equal(
     String(
       new VFileMessage(
         'xxx',
@@ -139,13 +154,13 @@ test('VFileMessage(reason[, place][, origin])', function (t) {
   const position = literalNode.position
   const m6 = new VFileMessage('test', position)
 
-  t.deepEqual(m6.position, position, 'should accept a position (1)')
-  t.equal(String(m6), '2:3-2:5: test', 'should accept a position (2)')
+  assert.deepEqual(m6.position, position, 'should accept a position (1)')
+  assert.equal(String(m6), '2:3-2:5: test', 'should accept a position (2)')
 
   const point = position.start
   const m7 = new VFileMessage('test', point)
 
-  t.deepEqual(
+  assert.deepEqual(
     m7.position,
     {
       start: point,
@@ -154,16 +169,16 @@ test('VFileMessage(reason[, place][, origin])', function (t) {
     'should accept a position (3)'
   )
 
-  t.equal(String(m7), '2:3: test', 'should accept a position (4)')
+  assert.equal(String(m7), '2:3: test', 'should accept a position (4)')
 
-  t.deepEqual(
+  assert.deepEqual(
     // @ts-expect-error
     new VFileMessage('test', {}).position,
     {start: {line: null, column: null}, end: {line: null, column: null}},
     'should ignore an empty object'
   )
 
-  t.equal(
+  assert.equal(
     // @ts-expect-error runtime supports an overload w/o position.
     new VFileMessage('test', 'charlie').ruleId,
     'charlie',
@@ -173,13 +188,11 @@ test('VFileMessage(reason[, place][, origin])', function (t) {
   // @ts-expect-error runtime supports an overload w/o position.
   const m8 = new VFileMessage('test', 'delta:echo')
 
-  t.deepEqual(
+  assert.deepEqual(
     [m8.source, m8.ruleId],
     ['delta', 'echo'],
     'should accept a `source` and `ruleId` in `origin`'
   )
-
-  t.end()
 })
 
 /**
